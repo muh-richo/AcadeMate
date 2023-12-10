580fcdb6d80448560f127d7544dc59aa1ec0966e
muh-richo <muhrichoa@gmail.com>
2023-12-10 18:48:17 +0700

    fix ui & backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,13 @@ buildscript {
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 
 //    firebase
     id("com.google.gms.google-services") version "4.4.0" apply false
 
 //    hilt
-    id ("com.android.library") version "8.1.1" apply false
+    id ("com.android.library") version "8.2.0" apply false
     id ("com.google.dagger.hilt.android") version "2.44" apply false
 }
